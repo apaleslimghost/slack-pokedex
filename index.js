399cb60c86d9905077155f25c3d3bbfecdf15e52
Matt Brennan <mattyb1000@gmail.com>
2016-07-15 13:53:07 +0100

    everything lol

---
+++ index.js
@@ -0,0 +1,35 @@
+import {send} from 'micro-core';
+import fetch from 'node-fetch';
+import isPokemon from '@quarterto/is-pokemon';
+import sanitizePokemonName from '@quarterto/sanitize-pokemon-name';
+import parseSlackBody from '@quarterto/slack-body';
+import {words} from 'lodash';
+
+const getMon = mon => fetch(`http://pokeapi.co/api/v2/pokemon/${mon}`);
+const getDex = mon => fetch(`http://pokeapi.co/api/v2/pokemon-species/${mon}`);
+
+const getFlavourText = pokedexEntry => pokedexEntry.flavor_text_entries.find(entry => entry.language.name === 'en' && entry.version.name === 'alpha-sapphire').flavor_text;
+
+const getName = pokedexEntry => pokedexEntry.names.find(entry => entry.language.name === 'en').name;
+
+export default async function(req, res) {
+	const {text} = await parseSlackBody(req);
+	const w = words(text);
+	const mons = w.map(sanitizePokemonName).filter(isPokemon);
+
+
+	if(mons.length) {
+		send(res, 200, {
+			text: ':lure-module:',
+			attachments: await Promise.all(mons.map(async mon => {
+				const data = await (await getMon(mon)).json();
+				const dex = await (await getDex(mon)).json();
+				return {
+					pretext: getName(dex),
+					image_url: (Math.random() < 1/8192 ? data.sprites.front_shiny : data.sprites.front_default),
+					text: getFlavourText(dex),
+			 	};
+			}))
+		});
+	} else send(res, 200, {});
+}
